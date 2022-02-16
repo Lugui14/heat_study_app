92f30766a265ea1796a8eedd258fe28841b68843
Lugui14 <zanelallopes9977@gmail.com>
2022-02-16 13:58:49 -0300

    button init

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Header } from '../../components/Header';
+import { MessageList } from '../../components/MessageList';
+import { SignInBox } from '../../components/SignInBox';
 
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>Olá React Native</Text>
+      <Header />
+      <MessageList />
+      <SignInBox />
     </View>
   )
 }
